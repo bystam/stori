9ea3ec03e44e7fc80b091b4b9afc976efcdb8946
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-06-06 20:20:31 +0200

    move some files

--- rails/app/controllers/story_posts_controller.rb
+++ rails/app/controllers/story_posts_controller.rb
@@ -1,25 +1,4 @@
 class StoryPostsController < ApplicationController
-  # GET /story_posts
-  # GET /story_posts.json
-  def index
-    @story_posts = StoryPost.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @story_posts }
-    end
-  end
-
-  # GET /story_posts/1
-  # GET /story_posts/1.json
-  def show
-    @story_post = StoryPost.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @story_post }
-    end
-  end
 
   # GET /story_posts/new
   # GET /story_posts/new.json
@@ -30,11 +9,6 @@ class StoryPostsController < ApplicationController
       format.html # new.html.erb
       format.json { render json: @story_post }
     end
-  end
-
-  # GET /story_posts/1/edit
-  def edit
-    @story_post = StoryPost.find(params[:id])
   end
 
   # POST /story_posts
@@ -51,34 +25,6 @@ class StoryPostsController < ApplicationController
         format.html { render action: "new" }
         format.json { render json: @story_post.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PUT /story_posts/1
-  # PUT /story_posts/1.json
-  def update
-    @story_post = StoryPost.find(params[:id])
-
-    respond_to do |format|
-      if @story_post.update_attributes(params[:story_post])
-        format.html { redirect_to @story_post, notice: 'Story post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @story_post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /story_posts/1
-  # DELETE /story_posts/1.json
-  def destroy
-    @story_post = StoryPost.find(params[:id])
-    @story_post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to story_posts_url }
-      format.json { head :no_content }
     end
   end
 end
--- rails/app/controllers/story_threads_controller.rb
+++ rails/app/controllers/story_threads_controller.rb
@@ -23,66 +23,6 @@ class StoryThreadsController < ApplicationController
     end
   end
 
-  # GET /story_threads/new
-  # GET /story_threads/new.json
-  def new
-    @story_thread = StoryThread.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @story_thread }
-    end
-  end
-
-  # GET /story_threads/1/edit
-  def edit
-    @story_thread = StoryThread.find(params[:id])
-  end
-
-  # POST /story_threads
-  # POST /story_threads.json
-  def create
-    @story_thread = StoryThread.new(params[:story_thread])
-
-    respond_to do |format|
-      if @story_thread.save
-        format.html { redirect_to @story_thread, notice: 'Story thread was successfully created.' }
-        format.json { render json: @story_thread, status: :created, location: @story_thread }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @story_thread.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /story_threads/1
-  # PUT /story_threads/1.json
-  def update
-    @story_thread = StoryThread.find(params[:id])
-
-    respond_to do |format|
-      if @story_thread.update_attributes(params[:story_thread])
-        format.html { redirect_to @story_thread, notice: 'Story thread was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @story_thread.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /story_threads/1
-  # DELETE /story_threads/1.json
-  def destroy
-    @story_thread = StoryThread.find(params[:id])
-    @story_thread.destroy
-
-    respond_to do |format|
-      format.html { redirect_to story_threads_url }
-      format.json { head :no_content }
-    end
-  end
-
   def self.current_thread
     current_last = StoryThread.last
     if current_last.nil? || current_last.end.past?
