be75d075634d4f1490310a2459ae90d36279873d
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-06-06 20:15:25 +0200

    everything works

--- rails/app/controllers/story_threads_controller.rb
+++ rails/app/controllers/story_threads_controller.rb
@@ -1,4 +1,6 @@
 class StoryThreadsController < ApplicationController
+  NEW_DAY_START = 5.hours
+
   # GET /story_threads
   # GET /story_threads.json
   def index
@@ -82,6 +84,28 @@ class StoryThreadsController < ApplicationController
   end
 
   def self.current_thread
-    return StoryThread.last
+    current_last = StoryThread.last
+    if current_last.nil? || current_last.end.past?
+      current_last = create_new_thread
+    end
+    return current_last
+  end
+
+  def self.create_new_thread
+    new_day_midnight = DateTime.now.beginning_of_day
+    new_start = new_day_midnight + 5.hours
+    new_end = new_start + 1.day
+    new_name = new_start.to_date.to_s
+
+    story_thread = StoryThread.new
+    story_thread.start = new_start
+    story_thread.end = new_end
+    story_thread.name = new_name
+
+    if story_thread.save
+      return story_thread
+    else
+      return nil
+    end
   end
 end
