f13e7295b0eda12786e71bdbdaab4a1c14c6da38
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-06-06 19:48:12 +0200

    liten commit

--- rails/app/models/story_post.rb
+++ rails/app/models/story_post.rb
@@ -1,5 +1,12 @@
 class StoryPost < ActiveRecord::Base
+  LAST_WORD_AMOUNT = 3
+
   attr_accessible :story_thread, :text
 
   belongs_to :story_thread
+
+  def last_words
+    last_words = text.split(" ").last(LAST_WORD_AMOUNT)
+    return last_words.join(" ")
+  end
 end
