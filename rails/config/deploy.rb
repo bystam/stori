f57588b8aa1f8218a5e1316e3b0db0f401db65d7
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-06-07 17:40:48 -0400

    New deploy files

--- rails/config/deploy.rb
+++ rails/config/deploy.rb
@@ -1,58 +1,50 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+set :application, "stori"
+set :repository, 'https://github.com/bystam/stori.git'
+set :scm, :git
 
-set :application, 'stori'
-set :repo_url, 'https://github.com/bystam/stori.git'
+set :branch, fetch(:branch, 'master')
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :stages, ['production']
+set :default_stage, 'production'
+require 'capistrano/ext/multistage'
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+webfactional_domain = 'studs.webfactional.com'
+role :app, webfactional_domain, :alias => 'webfactional'
+role :web, webfactional_domain, :alias => 'webfactional'
+role :db,  webfactional_domain, :alias => 'webfactional', :primary => true
 
-# Default value for :scm is :git
-# set :scm, :git
+set :user, "studs"
+set :scm_username, "bystam"
+set :use_sudo, false
+default_run_options[:pty] = true
 
-# Default value for :format is :pretty
-# set :format, :pretty
+set :rails_env, 'production'
+default_environment['RAILS_ENV'] = 'production'
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+# Bundler
+require 'bundler/capistrano'
 
-# Default value for :pty is false
-# set :pty, true
 
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+# Additional shared paths
+set :shared_children, shared_children + %w(public/uploads)
+set :shared_files, %w(config/database.yml)
+set(:shared_file_path) { shared_path }
+require 'capistrano/shared_file'
 
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
+  desc "Deploy"
+  task :default do
+    update
+    restart
+    clear_cache
   end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+  
+  desc "Restart nginx"
+  task :restart do
+    run "#{deploy_to}/bin/restart"
   end
 
 end
