632e0c1a89c8e40ef4ce867414073c75e5c18949
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-06-08 15:58:25 -0400

    adds secrets to repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stori::Application.config.secret_token = 'ac8c047422944e2b74c6f7e61472b92a3e4c156a4a8187f5c1c32ba0183a72e06fe72add1e3963c890a59fe674890ec1ed2fbbc53d400ab02950673f7906f939'
